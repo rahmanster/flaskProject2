3647b14ed3646a745019763d8bcd43fedc90aadb
EC2 Default User <ec2-user@ip-172-31-42-101.us-east-2.compute.internal>
2019-07-23 19:29:29 +0000

    potential decimal fix

--- app/models/model.py
+++ app/models/model.py
@@ -1,16 +1,22 @@
 def tip(num, tip):
     answer = num*(tip/100)
     answer = str(round(answer, 2))
+    if answer[len(answer)-1] == "0":
+        return answer+"0"
     return answer
     
 def totalBill(num,tip):
     answer= num*(1+(tip/100))
     answer = str(round(answer, 2))
+    if answer[len(answer)-1] == "0":
+        return answer+"0"
     return answer
     
 def split(num, tip, people):
     answer = (num*(1+(tip/100)))/people
     answer = str(round(answer, 2))
+    if answer[len(answer)-1] == "0":
+        return answer+"0"
     return answer
     
     
