3f1b70f487586468a5b540b67ca6407b173ab0bd
EC2 Default User <ec2-user@ip-172-31-42-101.us-east-2.compute.internal>
2019-07-23 18:22:08 +0000

    initial changes

---
+++ app/models/model.py
@@ -0,0 +1,7 @@
+def tip(num, tip):
+    return num*(tip/100)
+    
+def totalBill(num,tip):
+    return num*(1+(tip/100))
+    
+    
--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,17 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    userData = dict(request.form)
+    return render_template("index.html")
+        
+@app.route("/bill", methods = ["GET", "POST"])
+def bill():
+    if request.method == "GET":
+        return "You didn't fill out the form."
+    else:
+        userData = dict(request.form)
+        total = float(userData['total'][0])
+        tip = float(userData['tip'][0])
+        finalTotal = model.totalBill(total,tip)
+        finalTip = model.tip(total,tip)
+        return render_template("bill.html", finalTip = finalTip, finalTotal = finalTotal)
